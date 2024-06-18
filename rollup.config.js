574e109d5e4c26960a3e93b2c7f31c6d275bf864
Peter Tsivinsky <creatsiv@gmail.com>
2024-06-18 10:42:24 +0300

    feat: add TS support

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
+import typescript from '@rollup/plugin-typescript';
 
 const config = {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: {
     file: 'dist/index.esm.js',
     format: 'esm',
   },
-  external: [/@babel\/runtime/, 'react'],
+  external: [/@babel\/runtime/, 'react', 'tslib'],
   plugins: [
-    babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      rootDir: 'src',
+      exclude: ['**/*.test.ts', '**/*.test.tsx'],
+    }),
+    babel({ babelHelpers: 'runtime', extensions: ['.js', '.jsx', '.ts', '.tsx'], plugins: ['@babel/plugin-transform-runtime'] }),
     filesize(),
   ],
 };
